c7ae89c52a8ab2fa26208be74c2d0c1bea29f8b9
Jesse Millar <hellojessemillar@gmail.com>
2016-04-05 14:21:30 -0600

    Almost working telnet

--- server.go
+++ server.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
+	"github.com/ziutek/telnet"
 )
 
 func hello(c web.C, w http.ResponseWriter, r *http.Request) {
@@ -16,20 +17,52 @@ func hello(c web.C, w http.ResponseWriter, r *http.Request) {
 func getRoomByName(c web.C, w http.ResponseWriter, r *http.Request) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "http://lazyeye.byu.edu/fusion/apiservice/rooms?search="+c.URLParams["name"], nil)
-	check(err)
+	checkErr(err)
 
 	req.Header.Add("Content-Type", "application/json")
 	resp, err := client.Do(req)
-	check(err)
+	checkErr(err)
 
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	check(err)
+	checkErr(err)
 
 	fmt.Fprintf(w, "%s", body)
 }
 
-func check(err error) {
+func sendCommand(t *telnet.Conn, command string) {
+	buf := make([]byte, len(command)+1)
+	copy(buf, command)
+	buf[len(command)] = '\n'
+	_, err := t.Write(buf)
+	checkErr(err)
+}
+
+func telnetDial(c web.C, w http.ResponseWriter, r *http.Request) {
+	command := c.URLParams["command"]
+	t, err := telnet.Dial("tcp", "10.6.36.53:23")
+	checkErr(err)
+
+	// t.SetUnixWriteMode(true)
+
+	sendCommand(t, command)
+	// sendCommand(t, "^]")
+
+	_, err = t.ReadString('>')
+	checkErr(err)
+
+	data, err := t.ReadString('>')
+	checkErr(err)
+
+	t.Close() // Close the telnet session
+	fmt.Fprintf(w, "%s", data)
+}
+
+func log(message string) {
+	fmt.Printf("%s\n", message)
+}
+
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
@@ -38,5 +71,6 @@ func check(err error) {
 func main() {
 	goji.Get("/hello/:name", hello)
 	goji.Get("/room/:name", getRoomByName)
+	goji.Get("/telnet/:command", telnetDial)
 	goji.Serve()
 }
