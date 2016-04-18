b0c51ec1b5fc50e150075d804a7cd2fc9ad4202e
Jesse Millar <hellojessemillar@gmail.com>
2016-04-18 17:24:22 -0600

    Health checking works with the exception of not being able to ping anything not on the subnet

---
+++ helpers/health.go
@@ -0,0 +1,53 @@
+package helpers
+
+import (
+	"net"
+	"strings"
+	"time"
+)
+
+// Health represents the results of various health checks run on each box
+type Health struct {
+	PingIn  bool
+	PingOut bool
+}
+
+// GetHealth checks a number of status items for each box and returns a JSON object representing the test restults
+func GetHealth(address string) (Health, error) {
+	pingIn, err := checkPingIn(address)
+	if err != nil {
+		return Health{}, err
+	}
+
+	pingOut, err := checkPingOut(address)
+	if err != nil {
+		return Health{}, err
+	}
+
+	return Health{PingIn: pingIn, PingOut: pingOut}, nil
+}
+
+func checkPingIn(address string) (bool, error) {
+	timeout := 2 * time.Second
+
+	connection, err := net.DialTimeout("tcp", address+":23", timeout)
+	if err == nil {
+		connection.Close()
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func checkPingOut(address string) (bool, error) {
+	response, err := GetTelnetOutput(address, "DMPS-300-C>", "ping avmetrics1.byu.edu")
+	if err != nil {
+		return false, err
+	}
+
+	if strings.Contains(response, "alive") {
+		return true, nil
+	}
+
+	return false, nil
+}
