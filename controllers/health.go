d755bc8f174f1069c2d38b962a11f0f3b4138a36
Jesse Millar <hellojessemillar@gmail.com>
2016-05-13 12:02:24 -0600

    Update health.go

--- controllers/health.go
+++ controllers/health.go
@@ -7,5 +7,5 @@ import (
 )
 
 func Health(c echo.Context) error {
-	return c.String(http.StatusOK, "Uh, we had a slight weapons malfunction, but uh... everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
+	return c.JSON(http.StatusOK, "Uh, we had a slight weapons malfunction, but uh... everything's perfectly all right now. We're fine. We're all fine here now, thank you. How are you?")
 }
