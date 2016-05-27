2a4a8270db434d6a76251739ce83a0d6dc126e0a
Jesse Millar <hellojessemillar@gmail.com>
2016-05-27 14:33:24 -0600

    Beginning tests of deploying to AWS Beanstalk

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+PROJECT_NAME=$1
+SHA1=$2 # Nab the SHA1 of the desired build from a command-line argument
+EB_BUCKET=elasticbeanstalk-us-west-2-194925301021
+
+# Create new Elastic Beanstalk version
+DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+
+# Update Elastic Beanstalk environment to new version
+aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
