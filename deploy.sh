33dde0a4b9e02850244bc2ff3fdef3ce293f6066
Jesse Millar <hellojessemillar@gmail.com>
2016-06-06 16:25:47 -0600

    Adding a comment

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # Copy the Dockerrun file to the S3 bucket
 aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
