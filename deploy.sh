8c3ec23906ee2243f87088023af97202109a8fed
Dan Clegg <dan_clegg@byu.edu>
2016-06-07 21:58:41 -0600

    Updating application name with organization as prefix

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws configure set default.region us-west-2
 aws configure set region us-west-2
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE # Copy the Dockerrun file to the S3 bucket
-aws elasticbeanstalk create-application-version --application-name $PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name byuoitav-$PROJECT_NAME --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
 aws elasticbeanstalk update-environment --environment-name $PROJECT_NAME-env --version-label $SHA1
