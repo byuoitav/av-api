961a686d9d357271058eb3ceff3b0afdc06b36f8
Jesse Millar <hellojessemillar@gmail.com>
2016-06-15 16:55:17 -0600

    Moving to the standard http package to allow huge headers

--- server.go
+++ server.go
@@ -7,7 +7,7 @@ import (
 	"github.com/byuoitav/hateoas"
 	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/fasthttp"
+	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
 )
 
@@ -55,5 +55,5 @@ func main() {
 	router.Delete("/buildings/:building/rooms/:room/signals/:signal", controllers.UnimplementedResponse)
 
 	fmt.Printf("AV API is listening on %s\n", port)
-	router.Run(fasthttp.New(port))
+	router.Run(standard.New(port))
 }
