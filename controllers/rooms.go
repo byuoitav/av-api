f737af1a7e5231f0c7784f62adbb34be1f330f59
joseph <joseph.blodgett@gmail.com>
2016-08-22 15:20:43 -0600

    Change Display Input working?

--- controllers/rooms.go
+++ controllers/rooms.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -12,6 +13,8 @@ import (
 	"github.com/byuoitav/hateoas"
 	"github.com/labstack/echo"
 )
+
+var databaseLocation = "http://localhost:8006"
 
 func isRoomAvailable(room fusion.Room) (fusion.Room, error) {
 	available, err := helpers.IsRoomAvailable(room)
@@ -110,30 +113,41 @@ func GetRoomByNameAndBuilding(context echo.Context) error {
 	return nil
 }
 
-func getRoomByInfo(roomName string, buildingName string) (accessors.Room, error) {
-	resp, err := http.Get("http://localhost:8006/buildings/" + buildingName + "/rooms/" + roomName)
-	var toReturn accessors.Room
+func getData(url string, structToFill interface{}) error {
+	resp, err := http.Get(url)
 	if err != nil {
-		return toReturn, err
+		return err
 	}
 
 	b, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return toReturn, err
+		return err
 	}
 
-	err = json.Unmarshal(b, &toReturn)
-
+	err = json.Unmarshal(b, structToFill)
 	if err != nil {
-		return toReturn, err
+		return err
 	}
-	return toReturn, nil
+	return nil
+}
+
+func getRoomByInfo(roomName string, buildingName string) (accessors.Room, error) {
+	url := databaseLocation + "/buildings/" + buildingName + "/rooms/" + roomName
+	var toReturn accessors.Room
+	err := getData(url, &toReturn)
+	return toReturn, err
+}
+
+func getDeviceByName(roomName string, buildingName string, deviceName string) (accessors.Device, error) {
+	var toReturn accessors.Device
+	err := getData(databaseLocation+"/buildings/"+buildingName+"/ "+roomName+"/devices/"+deviceName, &toReturn)
+	return toReturn, err
 }
 
 func getDevicesByRoom(roomName string, buildingName string) ([]accessors.Device, error) {
 	var toReturn []accessors.Device
 
-	resp, err := http.Get("http://localhost:8006/buildings/ITB/1110/devices")
+	resp, err := http.Get(databaseLocation + "/buildings/" + buildingName + "/ " + roomName + "/devices")
 
 	if err != nil {
 		return toReturn, err
@@ -154,13 +168,16 @@ func getDevicesByRoom(roomName string, buildingName string) ([]accessors.Device,
 
 //PublicRoom is the struct that is returned (or put) as part of the public API
 type PublicRoom struct {
-	CurrentInput string
-	Displays     []Display
-	AudioDevices []AudioDevice
+	CurrentVideoInput string
+	CurrentAudioInput string
+	Displays          []Display
+	AudioDevices      []AudioDevice
 }
 
 //AudioDevice represents an audio device
 type AudioDevice struct {
+	Name   string
+	Power  string
 	Muted  bool
 	Volume int
 }
@@ -168,8 +185,49 @@ type AudioDevice struct {
 //Display represents a display
 type Display struct {
 	Name    string
-	Power   bool
+	Power   string
 	Blanked bool
+}
+
+func getDevicesByBuildingAndRoomAndRole(room string, building string, roleName string) ([]accessors.Device, error) {
+
+	resp, err := http.Get(databaseLocation + "/buildings/" + building + "/rooms/" + room + "/devices/role/" + roleName)
+	if err != nil {
+		return []accessors.Device{}, err
+	}
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []accessors.Device{}, err
+	}
+
+	var devices []accessors.Device
+	err = json.Unmarshal(b, &devices)
+	if err != nil {
+		return []accessors.Device{}, err
+	}
+
+	return devices, nil
+}
+
+func validateChangeInputSuppliedValue(deviceToCheck string, room string, building string, roleName string) (bool, error) {
+
+	if len(deviceToCheck) > 0 {
+		devices, err := getDevicesByBuildingAndRoomAndRole(room, building, roleName)
+		if err != nil {
+			return false, err
+		}
+		if len(devices) < 1 {
+			return false, errors.New("No " + roleName + " input devices in room.")
+		}
+
+		for _, val := range devices {
+			if strings.EqualFold(deviceToCheck, val.Name) || strings.EqualFold(deviceToCheck, val.Type) {
+				return true, nil
+			}
+		}
+	}
+	return false, errors.New("Invalid " + roleName + " devices sepecified.")
 }
 
 /*PutRoomChanges is the handler to accept puts to /buildlings/:buildling/rooms/:room
@@ -197,59 +255,72 @@ func PutRoomChanges(context echo.Context) error {
 	if err != nil {
 		return err
 	}
-	//TODO: Have logic here that checks what was passed in and only changes what is necessary.
-	if !strings.EqualFold(roomInQuestion.CurrentInput, "") {
-		return changeCurrentInput(roomInQuestion, room, building)
+
+	//This is to say if we want to set audio input even if devices are both A/V outputs.
+	//forceAudioChange := true
+	//if we're setting both, default to video.
+	if len(roomInQuestion.CurrentAudioInput) > 0 && len(roomInQuestion.CurrentVideoInput) > 0 {
+		//forceAudioChange = false
 	}
+
+	//TODO: Have logic here that checks what was passed in and only changes what is necessary.
+	has, err := validateChangeInputSuppliedValue(roomInQuestion.CurrentAudioInput, room, building, "VideoIn")
+	if err != nil {
+		return err
+	} else if has {
+		return changeCurrentVideoInput(roomInQuestion, room, building)
+	}
+
+	//has, err = validateChangePowerSuppliedValues()
+
 	return nil
 }
 
-func changeCurrentInput(room PublicRoom, roomName string, buildingName string) error {
+func changeCurrentVideoInput(room PublicRoom, roomName string, buildingName string) error {
 
 	//magic strings - we'll replace these in the endpoint path.
 	portToMatch := ":port"
 	commandName := "ChangeInput"
 
-	devices, err := getDevicesByRoom(roomName, buildingName)
-
+	devices, err := getDevicesByBuildingAndRoomAndRole(roomName, buildingName, "VideoOut")
 	if err != nil {
 		return err
 	}
 
-	//Get the output device. Assume FOR NOW it's a TV and will always be there.
-	var outputDevice accessors.Device
-	var inputDevice accessors.Device
+	inputDevice, err := getDeviceByName(roomName, buildingName, room.CurrentVideoInput)
+	if err != nil {
+		return err
+	}
+
 	for _, val := range devices {
-		if val.Output && val.Type == 1 {
-			outputDevice = val
+		var curCommand accessors.Command
+		for _, val := range val.Commands {
+			if strings.EqualFold(val.Name, commandName) {
+				curCommand = val
+			}
 		}
-		if strings.EqualFold(val.Name, room.CurrentInput) && val.Input {
-			inputDevice = val
+
+		//placeholder to be replaced by the value we get down below.
+		var portValue string
+		for _, val := range val.Ports {
+			if strings.EqualFold(val.Source, inputDevice.Name) {
+				portValue = val.Name
+			}
 		}
-	}
-	var curCommand accessors.Command
-	for _, val := range outputDevice.Commands {
-		if strings.EqualFold(val.Name, commandName) {
-			curCommand = val
+		if len(portValue) <= 0 {
+			//TODO: figure out error reporting here.
+			continue
 		}
-	}
 
-	portValue := "Hdmi1"
+		endpointPath := ReplaceIPAddressEndpoint(curCommand.Endpoint.Path, val.Address)
 
-	for _, val := range outputDevice.Ports {
-		if strings.EqualFold(val.Source, inputDevice.Name) {
-			portValue = val.Name
+		endpointPath = strings.Replace(endpointPath, portToMatch, portValue, -1)
+		//something to get the current port
+
+		_, err = http.Get("http://" + curCommand.Microservice + endpointPath)
+		if err != nil {
+			return err
 		}
-	}
-
-	endpointPath := ReplaceIPAddressEndpoint(curCommand.Endpoint.Path, outputDevice.Address)
-
-	endpointPath = strings.Replace(endpointPath, portToMatch, portValue, -1)
-	//something to get the current port
-
-	_, err = http.Get("http://" + curCommand.Microservice + endpointPath)
-	if err != nil {
-		return err
 	}
 
 	return nil
