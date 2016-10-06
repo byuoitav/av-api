407771c5041eeedfc684934fbb9bd9f28895f14f
joseph <joseph.blodgett@gmail.com>
2016-10-06 16:19:08 -0600

    Removing the cruft - making the API slim and sexy. Like a well crafted automobile.

--- handlers/rooms.go
+++ handlers/rooms.go
@@ -3,104 +3,10 @@ package handlers
 import (
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/byuoitav/av-api/helpers"
-	"github.com/byuoitav/av-api/packages/fusion"
-	"github.com/byuoitav/hateoas"
 	"github.com/labstack/echo"
 )
-
-func isRoomAvailable(room fusion.Room) (fusion.Room, error) {
-	available, err := helpers.IsRoomAvailable(room)
-	if err != nil {
-		return fusion.Room{}, err
-	}
-
-	room.Available = available
-
-	return room, nil
-}
-
-// GetAllRooms returns a list of all rooms Crestron Fusion knows about
-func GetAllRooms(context echo.Context) error {
-	allRooms, err := fusion.GetAllRooms()
-	if err != nil {
-		return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-	}
-
-	// Add HATEOAS links
-	for i := range allRooms.Rooms {
-		links, err := hateoas.AddLinks(context.Path(), []string{strings.Replace(allRooms.Rooms[i].Name, " ", "-", -1)})
-		if err != nil {
-			return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-		}
-
-		allRooms.Rooms[i].Links = links
-	}
-
-	return context.JSON(http.StatusOK, allRooms)
-}
-
-// GetRoomByName get a room from Fusion using only its name
-func GetRoomByName(context echo.Context) error {
-	room, err := fusion.GetRoomByName(context.Param("room"))
-	if err != nil {
-		return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-	}
-
-	links, err := hateoas.AddLinks(context.Path(), []string{context.Param("building"), context.Param("room")})
-	if err != nil {
-		return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-	}
-
-	room.Links = links
-
-	health, err := helpers.GetHealth(room.Address)
-	if err != nil {
-		return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-	}
-
-	room.Health = health
-
-	room, err = isRoomAvailable(room)
-	if err != nil {
-		return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-	}
-
-	return context.JSON(http.StatusOK, room)
-}
-
-// GetAllRoomsByBuilding pulls room information from fusion by building designator
-func GetAllRoomsByBuilding(context echo.Context) error {
-	allRooms, err := fusion.GetAllRooms()
-	if err != nil {
-		return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-	}
-
-	// Remove rooms that are not in the asked-for building
-	for i := len(allRooms.Rooms) - 1; i >= 0; i-- {
-		roomBuilding := strings.Split(allRooms.Rooms[i].Name, " ")
-
-		if roomBuilding[0] != context.Param("building") {
-			allRooms.Rooms = append(allRooms.Rooms[:i], allRooms.Rooms[i+1:]...)
-		}
-	}
-
-	// Add HATEOAS links
-	for i := range allRooms.Rooms {
-		room := strings.Split(allRooms.Rooms[i].Name, " ")
-
-		links, err := hateoas.AddLinks(context.Path(), []string{context.Param("building"), room[1]})
-		if err != nil {
-			return context.JSON(http.StatusBadRequest, helpers.ReturnError(err))
-		}
-
-		allRooms.Rooms[i].Links = links
-	}
-
-	return context.JSON(http.StatusOK, allRooms)
-}
 
 //GetRoomByNameAndBuildingHandler is almost identical to GetRoomByName
 func GetRoomByNameAndBuildingHandler(context echo.Context) error {
--- server.go
+++ server.go
@@ -28,27 +28,11 @@ func main() {
 
 	router.Get("/health", health.Check)
 
-	router.Get("/rooms", handlers.GetAllRooms, wso2jwt.ValidateJWT())
-	router.Get("/rooms/:room", handlers.GetRoomByName, wso2jwt.ValidateJWT())
-
 	// router.Get("/buildings", handlers.GetAllBuildings, wso2jwt.ValidateJWT())
-	router.Get("/buildings/:building", handlers.GetBuildingByName, wso2jwt.ValidateJWT())
-	router.Get("/buildings/:building/rooms", handlers.GetAllRoomsByBuilding, wso2jwt.ValidateJWT())
 	router.Get("/buildings/:building/rooms/:room", handlers.GetRoomByNameAndBuildingHandler, wso2jwt.ValidateJWT())
 
-	// POST requests
-	router.Post("/rooms", handlers.UnimplementedResponse, wso2jwt.ValidateJWT())
-	router.Post("/buildings", handlers.UnimplementedResponse, wso2jwt.ValidateJWT())
-
 	// PUT requests
-	router.Put("/rooms/:room", handlers.UnimplementedResponse, wso2jwt.ValidateJWT())
-	router.Put("/buildings/:building", handlers.UnimplementedResponse, wso2jwt.ValidateJWT())
 	router.Put("/buildings/:building/rooms/:room", handlers.SetRoomState, wso2jwt.ValidateJWT())
-
-	// DELETE requests
-	router.Delete("/rooms/:room", handlers.UnimplementedResponse, wso2jwt.ValidateJWT())
-	router.Delete("/buildings/:building", handlers.UnimplementedResponse, wso2jwt.ValidateJWT())
-	router.Delete("/buildings/:building/rooms/:room", handlers.UnimplementedResponse, wso2jwt.ValidateJWT())
 
 	log.Println("AV API is listening on " + port)
 	server := fasthttp.New(port)
