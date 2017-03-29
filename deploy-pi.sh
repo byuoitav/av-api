689b3c78a6e9a4ab69a6c7fce1f85765fcba493b
Jesse Millar <hellojessemillar@gmail.com>
2017-03-29 13:19:34 -0600

    Make the deployment script not require new environment variables

--- deploy-pi.sh
+++ deploy-pi.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [ $1 = "development" ]; then
-	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS_DEV"
+	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS"_development
 elif [ $1 = "stage" ]; then
-	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS_STAGE"
+	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS"_stage
 elif [ $1 = "production" ]; then
-	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS_PROD"
+	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS"
 else
 	echo "Either 'development', 'stage', or 'production' must be passed as the sole argument"
 fi
