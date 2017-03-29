fc4d726b0d294db7c5fb73419cb4a37fd1c4ce5b
Jesse Millar <hellojessemillar@gmail.com>
2017-03-29 11:46:17 -0600

    Typo in deploy-pi.sh

--- deploy-pi.sh
+++ deploy-pi.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-if [ $1 = "dev" ]; then
+if [ $1 = "development" ]; then
 	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS_DEV"
 elif [ $1 = "stage" ]; then
 	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS_STAGE"
 elif [ $1 = "production" ]; then
 	curl -X GET --header "Accept: application/json" --header "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_HEADER" "$RASPI_DEPLOYMENT_MICROSERVICE_WSO2_ADDRESS_PROD"
 else
-	echo "Either 'dev', 'stage', or 'production' must be passed as the sole argument"
+	echo "Either 'development', 'stage', or 'production' must be passed as the sole argument"
 fi
