e5d0e62f5e7897fd89c9cb12984c0d246009da92
Daniel Randall <dgrandall@me.com>
2017-05-04 11:49:48 -0600

    create an event to send

--- base/reporting.go
+++ base/reporting.go
@@ -7,34 +7,27 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/byuoitav/event-router-microservice/eventinfrastructure"
 )
 
 /*
 Event is the struct we push up to ELK.
 {
-  hostname: "",
-  timestamp: RFC 3339 Format,
-  localEnvironment: bool,
-  callingIP: "",
-  event: " ",
-  responseCode: int,
-  building: "",
-  room: ""
+	hostname: "",
+	timestamp: RFC 3339 Format,
+	localEnvironment: bool,
+	callingIP: "",
+	event: " ",
+	responseCode: int,
+	building: "",
+	room: ""
 }
 */
-type Event struct {
-	Hostname         string `json:"hostname,omitempty"`
-	Timestamp        string `json:"timestamp,omitempty"`
-	LocalEnvironment bool   `json:"localEnvironment,omitempty"`
-	Event            string `json:"event,omitempty"`
-	ResponseCode     int    `json:"responseCode,omitempty"`
-	Success          bool   `json:"success,omitempty"`
-	Building         string `json:"building,omitempty"`
-	Room             string `json:"room,omitempty"`
-	Device           string `json:"device,omitempty"`
-}
 
-func ReportToELK(e Event) error {
+// Reports an event to ELK
+// (depreciated) now reported by the event-translator-microservice
+func ReportToELK(e eventinfrastructure.Event) error {
 	var err error
 
 	e.Timestamp = time.Now().Format(time.RFC3339)
@@ -60,6 +53,38 @@ func ReportToELK(e Event) error {
 	log.Print("Sending event to: " + os.Getenv("ELASTIC_API_EVENTS"))
 
 	_, err = http.Post(os.Getenv("ELASTIC_API_EVENTS"),
+		"application/json",
+		bytes.NewBuffer(toSend))
+
+	return err
+}
+
+func Event(e eventinfrastructure.Event) error {
+	var err error
+
+	e.Timestamp = time.Now().Format(time.RFC3339)
+	if len(os.Getenv("LOCAL_ENVIRONMENT")) > 0 {
+		e.Hostname = os.Getenv("PI_HOSTNAME")
+	} else {
+		e.Hostname, err = os.Hostname()
+	}
+	if err != nil {
+		return err
+	}
+
+	e.LocalEnvironment = len(os.Getenv("LOCAL_ENVIRONMENT")) > 0
+
+	log.Printf("Event to send: %+v", e)
+
+	toSend, err := json.Marshal(&e)
+	if err != nil {
+		return err
+	}
+
+	log.Print("Sending event to event router.")
+
+	/* send error */
+	_, err = http.Post("hi",
 		"application/json",
 		bytes.NewBuffer(toSend))
 
