b9d13112aeade5865fea4009b8ca7700872b4ca1
Clark Green <clark_green@byu.edu>
2017-05-05 11:52:06 -0600

    adding status structs

--- status/status.go
+++ status/status.go
@@ -1,82 +1,29 @@
 package status
 
-import (
-	"log"
-	"strings"
-
-	"github.com/byuoitav/av-api/base"
-	"github.com/byuoitav/av-api/dbo"
-)
-
-const FLAG = "STATUS"
-const U_TYPE = "2"
-
-func GetAudioStatus(building string, room string) ([]base.AudioDevice, error) {
-
-	log.Printf("Getting status of audio devices")
-
-	const TYPE = "0"
-
-	audioDevices, err := dbo.GetDevicesByBuildingAndRoomAndRole(building, room, "AudioOut")
-	if err != nil {
-		return []base.AudioDevice{}, err
-	}
-
-	var outputs []base.AudioDevice
-	for _, device := range audioDevices {
-
-		var state base.AudioDevice
-		for _, command := range device.Commands {
-
-			if strings.HasPrefix(command.Name, FLAG) && (strings.HasSuffix(command.Name, TYPE) || strings.HasSuffix(command.Name, U_TYPE)) {
-
-				log.Printf("Querying state of device %s", device.Name)
-				//get microservice address
-				//get microserivce endpoint
-				//build url
-				//send request
-				//parse response
-			}
-
-			outputs = append(outputs, state)
-
-		}
-
-	}
-
-	return outputs, nil
+type PowerStatus struct {
+	Power string `json:"power",omitempty`
 }
 
-func GetDisplayStatus(building string, room string) ([]base.Display, error) {
+type BlankedStatus struct {
+	Blanked bool `json:"blanked",omitempty`
+}
 
-	log.Printf("Getting status of displays")
+type MuteStatus struct {
+	Muted bool `json:"muted",omitempty`
+}
 
-	const TYPE = "1"
+type VideoInput struct {
+	Input string `json:"input",omitempty`
+}
 
-	displays, err := dbo.GetDevicesByBuildingAndRoomAndRole(building, room, "VideoOut")
-	if err != nil {
-		return []base.Display{}, err
-	}
+type AudioInput struct {
+	Input string `json:"input",omitempty`
+}
 
-	var outputs []base.Display
-	for _, device := range displays {
+type AudioList struct {
+	Inputs []AudioInput `json"inputs",omitempty`
+}
 
-		var state base.Display
-		for _, command := range device.Commands {
-
-			log.Printf("Querying state of display %s", device.Name)
-			if strings.HasPrefix(command.Name, FLAG) && (strings.HasSuffix(command.Name, TYPE) || strings.HasSuffix(command.Name, U_TYPE)) {
-				//get microservice address
-				//get endpoint
-				//build url
-				//send request
-				//parse response
-			}
-		}
-
-		outputs = append(outputs, state)
-
-	}
-
-	return outputs, nil
+type VideoList struct {
+	Inputs []VideoInput `json:"inputs",omitemtpy`
 }
