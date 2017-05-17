a9406f5dfd9be19aa53a711af14dab802732e6c7
Clark Green <clark_green@byu.edu>
2017-05-17 13:04:38 -0600

    I wrote 6 lines of code. Great

--- status/input-default.go
+++ status/input-default.go
@@ -6,8 +6,8 @@ import (
 	"github.com/byuoitav/configuration-database-microservice/accessors"
 )
 
-const InputDefaultName = "STATUS_InputDefault"
-const InputDefaultCommandName = "STATUS_Input"
+const DEFAULT_INPUT_EVALUATOR = "STATUS_InputDefault"
+const DEFAULT_INPUT_COMMAND = "STATUS_Input"
 
 type InputDefault struct {
 }
@@ -17,13 +17,24 @@ func (p *InputDefault) GetDevices(room accessors.Room) ([]accessors.Device, erro
 }
 
 func (p *InputDefault) GenerateCommands(devices []accessors.Device) ([]StatusCommand, error) {
-	return generateStandardStatusCommand(devices, InputDefaultName, InputDefaultCommandName)
+	return generateStandardStatusCommand(devices, DEFAULT_INPUT_EVALUATOR, DEFAULT_INPUT_COMMAND)
 }
 
-func (p *InputDefault) EvaluateResponse(label string, value interface{}, Source accessors.Device, dest DestinationDevice) (string, interface{}, error) {
+func (p *InputDefault) EvaluateResponse(label string, value interface{}, source accessors.Device, dest DestinationDevice) (string, interface{}, error) {
 	log.Printf("Evaluating response: %s, %s in evaluator %v", label, value, BlankedDefaultName)
 
 	//we need to remap the port value to the device name, for this case, that's just the device plugged into that port, as defined in the port mapping
+
+	for _, port := range dest.Ports {
+
+		valueString, ok := value.(string)
+		if ok && port.Name == valueString {
+
+			value = port.Source
+
+		}
+
+	}
 
 	return label, value, nil
 }
