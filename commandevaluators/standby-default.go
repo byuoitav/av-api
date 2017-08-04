e68edd02cbd41c8ebf3d34452977fdb5e1ece0d4
dannyrandall <dgrandall@me.com>
2017-08-04 10:46:52 -0600

    equal fold, removed comments

--- commandevaluators/standby-default.go
+++ commandevaluators/standby-default.go
@@ -36,16 +36,16 @@ func (s *StandbyDefault) Evaluate(room base.PublicRoom) (actions []base.ActionSt
 			return
 		}
 
-		log.Printf("Setting power to 'standby' state for all output devices.")
+		log.Printf("Setting power to 'standby' state for all devices with a 'standby' power state.")
 		for _, device := range devices {
-
 			containsStandby := false
 			for _, ps := range device.PowerStates {
-				if ps == "Standby" {
+				if strings.EqualFold(ps, "Standby") {
 					containsStandby = true
 					break
 				}
 			}
+
 			if containsStandby {
 				log.Printf("Adding device %+v", device.Name)
 				eventInfo.Device = device.Name
@@ -57,20 +57,6 @@ func (s *StandbyDefault) Evaluate(room base.PublicRoom) (actions []base.ActionSt
 					EventLog:            []eventinfrastructure.EventInfo{eventInfo},
 				})
 			}
-			/*
-				if device.Output {
-
-					log.Printf("Adding device %+v", device.Name)
-					eventInfo.Device = device.Name
-					actions = append(actions, base.ActionStructure{
-						Action:              "Standby",
-						Device:              device,
-						GeneratingEvaluator: "StandbyDefault",
-						DeviceSpecific:      false,
-						EventLog:            []eventinfrastructure.EventInfo{eventInfo},
-					})
-				}
-			*/
 		}
 	}
 
