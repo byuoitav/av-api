f701457dd004f9dc8ec9ff037852879dffda3394
Joseph Blodgett <Joseph.blodgett@gmail.com>
2017-10-30 13:17:05 -0600

    build

--- statusevaluators/pathfinder/pathfinder.go
+++ statusevaluators/pathfinder/pathfinder.go
@@ -1,6 +1,7 @@
 package pathfinder
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/byuoitav/configuration-database-microservice/structs"
@@ -18,15 +19,15 @@ type Node struct {
 	Device structs.Device
 }
 
-func InitializeSignalPathfinder() SingalPathfinder {
-	sf := Signalpathfinder{
+func InitializeSignalPathfinder() SignalPathfinder {
+	sf := SignalPathfinder{
 		ForwardPath: make(map[string]map[string]bool),
 		ReversePath: make(map[string]map[string]bool),
 	}
 	return sf
 }
 
-func (sp *SingalPathfinder) AddEdge(Device structs.Device, port string) {
+func (sp *SignalPathfinder) AddEdge(Device structs.Device, port string) {
 	if Device.HasRole("videoswitcher") {
 		//we need to take the port as two ports an in and an out and create an edge from the source to the destination
 
@@ -38,15 +39,16 @@ func (sp *SingalPathfinder) AddEdge(Device structs.Device, port string) {
 		out := structs.Port{}
 
 		for _, p := range Device.Ports {
-			if p.Name == fmt.Spritnf("IN%v", splitPorts[0]) {
-				in := p
+			if p.Name == fmt.Sprintf("IN%v", splitPorts[0]) {
+				in = p
 				continue
 			}
 			if p.Name == fmt.Sprintf("OUT%v", splitPorts[1]) {
-				out := p
+				out = p
 				continue
 			}
 		}
+		fmt.Printf("%v, %v", in, out)
 
 		//check to see if we have a path we can expand
 
