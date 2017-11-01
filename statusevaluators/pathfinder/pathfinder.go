e83d056c614e8f3cad8adcab91ff7862bdc26f24
Joseph Blodgett <Joseph.blodgett@gmail.com>
2017-11-01 13:04:23 -0600

    you can get state back when you set state!!!

--- statusevaluators/pathfinder/pathfinder.go
+++ statusevaluators/pathfinder/pathfinder.go
@@ -1,17 +1,19 @@
 package pathfinder
 
 import (
-	"fmt"
+	"errors"
+	"log"
 	"strings"
 
 	"github.com/byuoitav/configuration-database-microservice/structs"
+	"github.com/fatih/color"
 )
 
-//In this device
-
 type SignalPathfinder struct {
-	ForwardPath map[string]map[string]bool
-	ReversePath map[string]map[string]bool
+	Devices  map[string]structs.Device
+	Expected int
+	Actual   int
+	Pending  map[string][]structs.Port // a list of the 'pending' ports. Really what this is is the fact that we can't add switches piecemeal into the graph
 }
 
 type Node struct {
@@ -19,41 +21,147 @@ type Node struct {
 	Device structs.Device
 }
 
-func InitializeSignalPathfinder() SignalPathfinder {
+func InitializeSignalPathfinder(devices []structs.Device, expected int) SignalPathfinder {
+	log.Printf("[Pathfinder] initializing pathfinder")
 	sf := SignalPathfinder{
-		ForwardPath: make(map[string]map[string]bool),
-		ReversePath: make(map[string]map[string]bool),
+		Expected: expected,
+		Pending:  make(map[string][]structs.Port),
+		Devices:  make(map[string]structs.Device),
+		Actual:   0,
+	}
+
+	for _, dev := range devices {
+		if _, ok := sf.Devices[dev.Name]; !ok {
+			sf.Devices[dev.Name] = dev
+		}
 	}
 	return sf
 }
 
-func (sp *SignalPathfinder) AddEdge(Device structs.Device, port string) {
-	if Device.HasRole("videoswitcher") {
-		//we need to take the port as two ports an in and an out and create an edge from the source to the destination
+//we need to store the state so that we can later use it to trace the value
+func (sp *SignalPathfinder) AddEdge(Device structs.Device, port string) bool {
 
-		//TODO: Handle the case where a swich is plugged into another switcher
+	log.Printf(color.HiCyanString("[Pathfinder] Adding edge :%v %v", Device.Name, port))
+	//we need to get the port from the list of devices
 
-		splitPorts := strings.Split(port, ":")
+	//go through the ports
+	realPort := structs.Port{}
 
-		in := structs.Port{}
-		out := structs.Port{}
+	if Device.HasRole("VideoSwitcher") {
+		split := strings.Split(port, ":")
+		//do the OUT port
+		outPort := "OUT" + split[1]
+		inPort := "IN" + split[0]
 
+		realPort.Name = port
+
+		//we have to build the port based on the in and out ports.
 		for _, p := range Device.Ports {
-			if p.Name == fmt.Sprintf("IN%v", splitPorts[0]) {
-				in = p
-				continue
+			if p.Name == outPort {
+				realPort.Destination = p.Destination
 			}
-			if p.Name == fmt.Sprintf("OUT%v", splitPorts[1]) {
-				out = p
-				continue
+			if p.Name == inPort {
+				realPort.Source = p.Source
+				realPort.Host = p.Host
 			}
 		}
-		fmt.Printf("%v, %v", in, out)
-
-		//check to see if we have a path we can expand
-
-		//check forward path
-		//val, ok := sp.ForwardPath[out.Destination]
-
+	} else {
+		//we can just use the port itself
+		for _, p := range Device.Ports {
+			if p.Name == port {
+				realPort = p
+			}
+		}
 	}
+
+	if _, ok := sp.Pending[Device.Name]; !ok {
+		sp.Pending[Device.Name] = []structs.Port{realPort}
+	} else {
+		sp.Pending[Device.Name] = append(sp.Pending[Device.Name], realPort)
+	}
+
+	sp.Actual++
+	if sp.Actual >= sp.Expected {
+		return true
+	}
+	return false
+}
+
+//returns a map of output -> input of all available paths.
+//we assume that there is an entry for each output device - and will trace back as far as we can through that route
+//we assume that all the 'edges' have been added
+func (sp *SignalPathfinder) GetInputs() (map[string]structs.Device, error) {
+	log.Printf(color.HiCyanString("[Pathfinder] Getting all inputs"))
+
+	toReturn := make(map[string]structs.Device)
+
+	log.Printf(color.HiCyanString("[Pathfinder] Devices: %v", len(sp.Devices)))
+	//we need to go through and find all of our output devices - then
+	for k, v := range sp.Devices {
+		_, ok := sp.Pending[k]
+		if !v.Output || !ok {
+			continue
+		}
+		log.Printf(color.HiCyanString("[Pathfinder] Tracing input for %v", k))
+
+		//we now trace his path back as far as we can
+		curDevice := k
+		prevDevice := ""
+
+		for !sp.Devices[curDevice].Input && curDevice != prevDevice {
+			next, err := sp.getNextDeviceInPath(curDevice, prevDevice)
+			if err != nil {
+				return toReturn, err
+			}
+			prevDevice = curDevice
+			curDevice = next
+			log.Printf(color.HiCyanString("[Pathfinder] Path includes %v -> %v", prevDevice, curDevice))
+		}
+		log.Printf(color.HiCyanString("[Pathfinder] Path ended. Final is %v -> %v  ", k, curDevice))
+		toReturn[k] = sp.Devices[curDevice]
+	}
+	return toReturn, nil
+}
+
+func (sp *SignalPathfinder) getNextDeviceInPath(curDevice string, lastDevice string) (string, error) {
+
+	//check to see if the current device has a port in the array
+	if _, ok := sp.Pending[curDevice]; !ok {
+		//it doesn't have an entry. Return
+		return curDevice, nil
+	}
+
+	array := sp.Pending[curDevice]
+
+	if len(array) == 0 {
+		return curDevice, nil
+	}
+	if len(array) == 1 {
+		//we can just return the device
+		return array[0].Source, nil
+	}
+
+	dev := sp.Devices[curDevice]
+
+	//we have multiple entries for the device, check if it's a vs, if not it's an error
+	if !dev.HasRole("VideoSwitcher") {
+		log.Printf(color.HiRedString("Non video switcher has multiple entries in the table, invalid state."))
+		return "", errors.New("Non video switcher has multiple entries in the table, invalid state.")
+	}
+	if len(lastDevice) == 0 {
+		msg := "Invalid state, videoswitcher evaluated as first in chain"
+		log.Printf(color.HiRedString(msg))
+		return "", errors.New(msg)
+	}
+
+	//it's a video switcher - so we need to figure out which of the pending ports we're talking about
+	for _, v := range array {
+		if v.Destination == lastDevice {
+			//we return the source
+			return v.Source, nil
+		}
+	}
+
+	//no path forward
+	return curDevice, nil
 }
